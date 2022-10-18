293a1c7e72094f44795738ca581cab6b7b3c2be0
Toran Mallow <toran.mallow@gmail.com>
2022-10-17 19:53:21 -0500

    Make floating electric poles provide electricity around them

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,33 +1,7 @@
-local vanilla_lamp = data.raw["lamp"]["small-lamp"]
-vanilla_lamp.fast_replaceable_group = "lamp"
+-- [ Allow Floating Electric Poles to provide electricity around them like a substation ]
+if (mods or script.active_mods)["cargo-ships"] then
 
--- Add a Floating Lamp
-local floating_lamp = util.table.deepcopy(vanilla_lamp)
-floating_lamp.name = "floating-lamp"
-floating_lamp.minable.result = "floating-lamp"
-floating_lamp.glow_size = 30
-floating_lamp.glow_color_intensity = 1
-floating_lamp.darkness_for_all_lamps_on = 0.3
-floating_lamp.darkness_for_all_lamps_off = 0.1
-floating_lamp.glow_render_mode = "multiplicative"
-floating_lamp.fast_replaceable_group = "lamp"
-data:extend({floating_lamp})
+   local floating_pole = data.raw["electric-pole"]["floating-electric-pole"]
+   floating_pole.supply_area_distance = data.raw["electric-pole"]["substation"].supply_area_distance
 
-data:extend({
-      {
-	 type = "recipe",
-	 name = "floating-lamp",
-	 enabled = false,
-	 ingredients =
-	    {
-	       {"electronic-circuit", 1},
-	       {"copper-cable", 3},
-	       {"empty-barrel", 4},
-	       {"iron-plate", 1}
-	    },
-	 result = "floating-lamp",
-	 result_count = 1
-      },
-})
-
-
+end
---
+++ data.lua
@@ -0,0 +1,37 @@
+-- Declare the floating lamp item
+data:extend{
+  {
+    type = "item",
+    name = "floating-lamp",
+    icon = "__base__/graphics/icons/small-lamp.png",
+    icon_size = 64, icon_mipmaps = 4,
+    subgroup = "circuit-network",
+    order = "a[light]-a[small-lamp-floating]",
+    place_result = "floating-lamp",
+    stack_size = 50
+  },
+}
+
+local vanilla_lamp = data.raw["lamp"]["small-lamp"]
+vanilla_lamp.fast_replaceable_group = "lamp"
+
+-- Add a floating lamp entity
+local floating_lamp = util.table.deepcopy(vanilla_lamp)
+floating_lamp.name = "floating-lamp"
+floating_lamp.minable.result = "floating-lamp"
+floating_lamp.glow_size = 30
+floating_lamp.glow_color_intensity = 1
+floating_lamp.darkness_for_all_lamps_on = 0.3
+floating_lamp.darkness_for_all_lamps_off = 0.1
+floating_lamp.glow_render_mode = "multiplicative"
+floating_lamp.fast_replaceable_group = "lamp"
+floating_lamp.collision_mask = {'ground-tile', 'object-layer'}
+floating_lamp.maximum_wire_distance = 48
+data:extend({floating_lamp})
+
+-- Add floating lamp recipe
+local floating_lamp_recipe = util.table.deepcopy(data.raw["recipe"]["small-lamp"])
+floating_lamp_recipe.name = "floating-lamp"
+floating_lamp_recipe.result = "floating-lamp"
+floating_lamp_recipe.enabled = true
+data:extend({floating_lamp_recipe})
